ef8dc09c1aa185e6e2e10ec9864182aa8009c20b
KikiopeShodipe <gilliansifuma@gmail.com>
2025-02-15 12:58:44 +0100

    Shopping list

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addTask(){
     let taskInput = document.getElementById("taskInput");
     let taskList = document.getElementById("taskList");
     if (taskInput.value.trim() === "") {
-        alert("Please enter a task!");
+        alert("Please enter a shopping list!");
         return;
     }
     let li = document.createElement("li");
